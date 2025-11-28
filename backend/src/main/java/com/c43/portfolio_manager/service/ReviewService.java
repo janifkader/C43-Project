ae9cfeab6b19f9b0ae2bf6519e778a51b3ea884d
Akshat Patel <akshatpatel1303@gmail.com>
2025-11-28 16:51:44 -0500

    Completed Review

--- backend/src/main/java/com/c43/portfolio_manager/service/ReviewService.java
+++ backend/src/main/java/com/c43/portfolio_manager/service/ReviewService.java
@@ -1,27 +1,29 @@
 package com.c43.portfolio_manager.service;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.stereotype.Service;
-
 import com.c43.portfolio_manager.model.Review;
 import com.c43.portfolio_manager.repository.ReviewRepo;
 
+
 @Service
 public class ReviewService {
-	
-	private ReviewRepo repo = new ReviewRepo();
-	
-	public int createReview(int user_id, int sl_id, String text) {
-		return repo.createReview(user_id, sl_id, text);
-	}
-	
-	public List<Review> getReviews(int sl_id) {
-		return repo.getReviews(sl_id);
-	}
+    
+    private ReviewRepo repo = new ReviewRepo();
+    
+    public int createReview(int user_id, int sl_id, String text) {
+        return repo.createReview(user_id, sl_id, text);
+    }
+    
+    public List<Review> getReviews(int sl_id) {
+        return repo.getReviews(sl_id);
+    }
+    
+    public boolean deleteReview(int review_id, int user_id) {
+        return repo.deleteReview(review_id, user_id);
+    }
+    
+    public boolean editReview(int review_id, int user_id, String new_text) {
+        return repo.editReview(review_id, user_id, new_text);
+    }
 }
